ab32a25f9c0380d472c53353a4958967c5853bfe
Jakub Jóźwiak <kubencki@gmail.com>
2020-02-25 00:04:18 +0100

    Master dev (#1)
    * add stf-player repo
    
    * add stf-player repo
    
    * add stf-player repo
    
    * remove stf-player form workspaces
    
    * add stf-player repo
    
    * add stf-player repo
    
    * add stf-player repo
    
    * remove stf-player form workspaces
    
    * fix
    
    * fix
    
    * fix
    
    * add working workspace with stf-player
    
    * add cors options and change component of creating teams for old
    
    * remove env

--- stf-player/src/models/teams/Create.js
+++ stf-player/src/models/teams/Create.js
@@ -84,96 +84,96 @@ const Input = ({
   />
 )
 
-const TeamCreate = ({ classes, ...rest }) => {
-  const [loading, setLoading] = React.useState(false)
-
-  const validate = (values) => {
-    const errors = { username: undefined, password: undefined }
-
-    if (!values.username) {
-      errors.username = 'Required'
-    }
-    return errors
-  }
-
-  const submit = values => {
-    setLoading(true)
-    console.log(values)
-    setLoading(false)
-  }
-
-  return (
-    <Form
-      onSubmit={submit}
-      validate={validate}
-      render={({ handleSubmit }) => (
-        <form onSubmit={handleSubmit} noValidate>
-          <Field
-            autoFocus
-            id='username'
-            name='username'
-            component={Input}
-            label={'Email'}
-            disabled={loading}
-          />
-          <Button
-            variant='contained'
-            type='submit'
-            color='primary'
-            disabled={loading}
-            className={classes.button}
-          >
-            {
-              loading &&
-              <div className={classes.loadingBar} >
-                <CircularProgress size={15} thickness={2} />
-              </div>
-            }
-            Create
-          </Button>
-        </form>
-      )}
-    />
-  )
-}
-
-// const TeamCreate = (props) => {
+// const TeamCreate = ({ classes, ...rest }) => {
+//   const [loading, setLoading] = React.useState(false)
+//
+//   const validate = (values) => {
+//     const errors = { username: undefined, password: undefined }
+//
+//     if (!values.username) {
+//       errors.username = 'Required'
+//     }
+//     return errors
+//   }
+//
+//   const submit = values => {
+//     setLoading(true)
+//     console.log(values)
+//     setLoading(false)
+//   }
+//
 //   return (
-//     <Create
-//       {...props}
-//       actions={<CreateActions />}
-//       title={'Team Create'}
-//     >
-//       <SimpleForm>
-//         <RadioButtonGroupInput
-//           source='type'
-//           optionText={<FullNameField />}
-//           choices={[
-//             { id: 'single', name: 'single' },
-//             { id: 'pair', name: 'pair' }
-//           ]}
-//         />
-//         <TextInput
-//           source={models.teams.fields.name}
-//           validate={required()}
-//         />
-//         <ReferenceArrayInput
-//           source={models.teams.fields.players}
-//           reference={constants.resources.players}
-//           validate={required()}
-//           label={'Team mate'}
-//           sort={{
-//             field: models.players.fields.email,
-//             order: 'ASC'
-//           }}
-//           filterToQuery={searchText => ({ [`${models.players.fields.email}.$regex`]: searchText })}
-//         >
-//           <Test />
-//         </ReferenceArrayInput>
-//       </SimpleForm>
-//     </Create>
+//     <Form
+//       onSubmit={submit}
+//       validate={validate}
+//       render={({ handleSubmit }) => (
+//         <form onSubmit={handleSubmit} noValidate>
+//           <Field
+//             autoFocus
+//             id='username'
+//             name='username'
+//             component={Input}
+//             label={'Email'}
+//             disabled={loading}
+//           />
+//           <Button
+//             variant='contained'
+//             type='submit'
+//             color='primary'
+//             disabled={loading}
+//             className={classes.button}
+//           >
+//             {
+//               loading &&
+//               <div className={classes.loadingBar} >
+//                 <CircularProgress size={15} thickness={2} />
+//               </div>
+//             }
+//             Create
+//           </Button>
+//         </form>
+//       )}
+//     />
 //   )
 // }
+
+const TeamCreate = (props) => {
+  return (
+    <Create
+      {...props}
+      actions={<CreateActions />}
+      title={'Team Create'}
+    >
+      <SimpleForm>
+        <RadioButtonGroupInput
+          source='type'
+          optionText={<FullNameField />}
+          choices={[
+            { id: 'single', name: 'single' },
+            { id: 'pair', name: 'pair' }
+          ]}
+        />
+        <TextInput
+          source={models.teams.fields.name}
+          validate={required()}
+        />
+        <ReferenceArrayInput
+          source={models.teams.fields.players}
+          reference={constants.resources.players}
+          validate={required()}
+          label={'Team mate'}
+          sort={{
+            field: models.players.fields.email,
+            order: 'ASC'
+          }}
+          filterToQuery={searchText => ({ [`${models.players.fields.email}.$regex`]: searchText })}
+        >
+          <Test />
+        </ReferenceArrayInput>
+      </SimpleForm>
+    </Create>
+  )
+}
 
 const enhance = compose(
   withStyles(styles)
